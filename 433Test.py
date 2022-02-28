44a793dd373b61584402e0002b96ec0a35ce5bbc
Equinox86 <scarras86@gmail.com>
2022-02-28 14:56:08 +0000

    App update

--- 433Test.py
+++ 433Test.py
@@ -7,8 +7,22 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(txPin, GPIO.OUT)
 GPIO.output(txPin, GPIO.LOW)
 
+# 15us pulse example
+pulseWidth = .00150
+longOn = pulseWidth * .75
+shortOn = pulseWidth * .25
+
 while(True):
-	sleep(.0625)
+
+	# Long Pulse
 	GPIO.output(txPin, GPIO.HIGH)
-	sleep(.0625)
+	sleep(longOn)
 	GPIO.output(txPin, GPIO.LOW)
+	sleep (pulseWidth - longOn)
+
+	# Short Pulse
+	GPIO.output(txPin, GPIO.HIGH)
+	sleep(shortOn)
+	GPIO.output(txPin, GPIO.LOW)
+	sleep (pulseWidth - shortOn)
+
--- Pi433.py
+++ Pi433.py
@@ -1,34 +1,87 @@
 import RPi.GPIO as GPIO
 import argparse
 import time
+import signal
+from Pulse import Pulse
+from copy import deepcopy
 
 LISTEN_PIN = 13
+signal_detected = False
 
-class Pulse:
-    def __init__(self, start, stop):
-        self.start = start
-        self.stop = stop
+currentPulse = Pulse()
+pulseQueue = [currentPulse]
+
+logfile = open("logfile.csv", "w+")
+
+def signalHandler(signum, frame):
+    GPIO.cleanup(LISTEN_PIN)
+    logfile.close()
+    print("\nExiting")
+    exit(0)
+
+def pulseCopy(src):
+    dest = Pulse()
+    dest.rise = src.rise
+    dest.fall = src.fall 
+    dest.period = src.period
+    dest.width = src.width
+
+    return dest
+
+def pulseDetect(LISTEN_PIN):
+
+    if(GPIO.input(LISTEN_PIN) == GPIO.HIGH):
+        # Get the rise time of the next pulse
+        rise = time.time()
+
+        # Calculate the period of the current pulse
+        currentPulse.period = rise - currentPulse.rise
+        currentPulse.width = currentPulse.fall - currentPulse.rise
+
+        # Add Pulse to the process queue
+        qPulse = pulseCopy(currentPulse)
+        
+        # Noise filter
+        # if (qPulse.period > .01):
+        pulseQueue.append(qPulse)
+
+        # Update Current Pulse with the rise time of the next pulse
+        currentPulse.rise = rise
+
+    else:
+        # Get the time of the falling edge
+        fall = time.time()
+        currentPulse.fall = fall
 
 if __name__ == '__main__':
-    
+
     # Parse args
     parser = argparse.ArgumentParser(description="Listens and sends codes on the 433Mhz band")
     parser.add_argument("-l", "--listen", help="Listen for codes", action="store_true")
     parser.add_argument("-c", "--code", help="Code to transmit")
     args = parser.parse_args()
-    # print(args)
+
 
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(LISTEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+    #Signal
+    signal.signal(signal.SIGINT, signalHandler)
+
     # Listen Flag
-    if(args.listen): 
-        
-        while(True): 
-            GPIO.wait_for_edge(LISTEN_PIN, GPIO.RISING)
-            rise_time = (time.time()*1000)
-            GPIO.wait_for_edge(LISTEN_PIN, GPIO.FALLING)
-            fall_time = (time.time()*1000)
+    if(args.listen):
+        # Pin Event detect and Callback
+        GPIO.add_event_detect(LISTEN_PIN, GPIO.BOTH)
+        GPIO.add_event_callback(LISTEN_PIN, pulseDetect)
 
+    # Main Loop
+    while(True):
+        time.sleep(2)
+        # Process the pulse queue
+        pulseCnt = len(pulseQueue)
+        print(pulseCnt)
 
-    # Send code
+        for i in range(0,pulseCnt):
+            sigPulse = pulseQueue.pop(0)
+            print(str(i) + ":" +  str(sigPulse.period) + ":" + str((sigPulse.width)/sigPulse.period))
+    
